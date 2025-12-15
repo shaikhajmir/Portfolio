db389bec7f228f12c6456caf5ef0c66f36b067ad
shaikhajmir <shaikhajmirilal8@gmail.com>
2025-12-15 13:07:52 +0530

    fix media serving and image upload

--- shaikh_portfolio/urls.py
+++ shaikh_portfolio/urls.py
@@ -16,3 +16,7 @@ urlpatterns = [
 # Serve MEDIA files during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
